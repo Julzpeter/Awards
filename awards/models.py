0753113e6b7a095798ee65e9f9e19cf1c7be4ff7
Juliet <chepngetichjuliet@gmail.com>
2019-07-31 15:19:49 +0300

    creates Profile class in models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    bio = models.CharField(max_length=200)
+    bio = models.CharField(max_length=200)
+    profile_pic = models.ImageField(
+        upload_to='photos/', default='photos/default.jpg')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    contact = models.CharField(max_length=12)
