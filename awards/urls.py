da54159e4c9a29dd8e424264499db448710c8072
Juliet <chepngetichjuliet@gmail.com>
2019-07-31 16:50:35 +0300

    configures the media

--- awards/urls.py
+++ awards/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.home,name='home'),
     url(r'^accounts/profile',views.home,name='home'),
+    url(r'^profile/(\d+)$', views.profile, name='profile'),
+    url(r'^profile/update/(\d+)$', views.update_profile, name='update_profile'),
+    url(r'^project/(\d+)$', views.single_project, name='project'),
+    url(r'^rating/(\d+)$', views.review_rating, name="review"),
+    url(r'^new/(\d+)$', views.new_project, name="new_project"),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- awardsapp/settings.py
+++ awardsapp/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
