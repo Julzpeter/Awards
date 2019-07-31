ec146e5f609fbcefdd9fb96dcf0f11d382ca08de
Juliet <chepngetichjuliet@gmail.com>
2019-07-31 17:11:15 +0300

    corrects indentation in views

--- awards/views.py
+++ awards/views.py
@@ -67,68 +67,74 @@ def update_profile(request, id):
     return render(request, 'profile/update_profile.html', {'user': user, 'form': form})
 
     # view function for a single_project
-    def single_project(request, c_id):
-        current_user = request.user
-        current_project = Project.objects.get(id=c_id)
-        ratings = Rating.objects.filter(project_id=c_id)
-        usability = Rating.objects.filter(
+
+
+def single_project(request, c_id):
+    current_user = request.user
+    current_project = Project.objects.get(id=c_id)
+    ratings = Rating.objects.filter(project_id=c_id)
+    usability = Rating.objects.filter(
         project_id=c_id).aggregate(Avg('usability_rating'))
-        content = Rating.objects.filter(
+    content = Rating.objects.filter(
         project_id=c_id).aggregate(Avg('content_rating'))
-        design = Rating.objects.filter(
+    design = Rating.objects.filter(
         project_id=c_id).aggregate(Avg('design_rating'))
-        return render(request, 'project.html', {"project": current_project, "user": current_user, 'ratings': ratings, "design": design, "content": content, "usability": usability})
+    return render(request, 'project.html', {"project": current_project, "user": current_user, 'ratings': ratings, "design": design, "content": content, "usability": usability})
 
-    def review_rating(request, id):
-        current_user = request.user
-        current_project = Project.objects.get(id=id)
-        if request.method == 'POST':
-            form = ProjectRatingForm(request.POST)
-            if form.is_valid():
-                rating = form.save(commit=False)
-                rating.project = current_project
-                rating.user = current_user
-                rating.save()
-                return redirect('project', id)
-        else:
-            form = ProjectRatingForm()
 
-        return render(request, 'rating/rating.html', {'form': form, "project": current_project, "user": current_user})
+def review_rating(request, id):
+    current_user = request.user
+    current_project = Project.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProjectRatingForm(request.POST)
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.project = current_project
+            rating.user = current_user
+            rating.save()
+            return redirect('project', id)
+    else:
+        form = ProjectRatingForm()
 
-    def new_project(request, id):
-        current_user = request.user
-        try:
-            profile = Profile.objects.get(user_id=id)
-        except ObjectDoesNotExist:
-            return redirect(update_profile, current_user.id)
+    return render(request, 'rating/rating.html', {'form': form, "project": current_project, "user": current_user})
 
-        if request.method == 'POST':
-            form = SubmitProjectForm(request.POST, request.FILES)
-            if form.is_valid():
-                project = form.save(commit=False)
-                project.owner = current_user
-                project.profile = Profile.objects.get(user_id=id)
-                project.save()
-                return redirect(home)
+
+def new_project(request, id):
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(user_id=id)
+    except ObjectDoesNotExist:
+        return redirect(update_profile, current_user.id)
+
+    if request.method == 'POST':
+        form = SubmitProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = current_user
+            project.profile = Profile.objects.get(user_id=id)
+            project.save()
+            return redirect(home)
+
         else:
             form = SubmitProjectForm()
 
         return render(request, 'new_project.html', {'form': form, "user": current_user})
 
-    def search_results(request):
+def search_results(request):
     if request.method == 'GET':
+
         query = request.GET.get('q')
 
         submitbutton = request.GET.get('submit')
 
         if query is not None:
             lookups = Q(sitename__icontains=query) | Q(
-                content__icontains=query)
+                    content__icontains=query)
 
             searched_project = Project.objects.filter(lookups).distinct()
 
             context = {'results': results,
-                       'submitbutton': submitbutton}
+                           'submitbutton': submitbutton}
 
             return render(request, 'searched.html', {"project": searched_project})
 
@@ -169,7 +175,3 @@ class ProjectList(APIView):
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
         permission_classes = (IsAdminOrReadOnly,)
-
-
-
-    
