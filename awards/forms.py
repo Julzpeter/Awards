df6dce4730e349aa91492875df4f2a86f916f51b
Juliet <chepngetichjuliet@gmail.com>
2019-07-31 16:25:25 +0300

    adds forms and imports in the views

--- awards/forms.py
+++ awards/forms.py
@@ -1,1 +1,21 @@
 from django import forms
+from .models import Profile, Project, Rating
+from django.contrib.auth.models import User
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+
+class ProjectRatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        exclude = ['project', 'pub_date', 'user']
+
+
+class SubmitProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['owner', 'pub_date', 'profile']
--- awards/views.py
+++ awards/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Project, Profile, Rating
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import SubmitProjectForm, ProjectRatingForm, UpdateProfileForm
 # Create your views here.
 
 
@@ -15,27 +16,45 @@ def home(request):
     return render(request, 'index.html', {'users': users, 'user': current, 'projects': projects})
 
     # view function for users profile page
+
+
 @login_required(login_url='/accounts/login/')
 def profile(request, id):
-        current_user = request.user
-        user = User.objects.get(id=id)
+    current_user = request.user
+    user = User.objects.get(id=id)
+    projects = Project.objects.filter(owner_id=id)
+    if current_user.id == user.id:
         projects = Project.objects.filter(owner_id=id)
-        if current_user.id == user.id:
-            projects = Project.objects.filter(owner_id=id)
-        current_user = request.user
-        user = User.objects.get(id=id)
+    current_user = request.user
+    user = User.objects.get(id=id)
+    try:
+        profile = Profile.objects.get(user_id=id)
+    except ObjectDoesNotExist:
+        return redirect(update_profile, current_user.id)
+    else:
         try:
+
             profile = Profile.objects.get(user_id=id)
         except ObjectDoesNotExist:
-            return redirect(update_profile, current_user.id)
-        else:
-            try:
 
-                profile = Profile.objects.get(user_id=id)
-            except ObjectDoesNotExist:
+            return redirect(no_profile, id)
 
-                return redirect(no_profile, id)
+    return render(request, 'profile/profile.html', {'user': user, 'profile': profile, 'current_user': current_user, "projects": projects})
 
-        return render(request, 'profile/profile.html', {'user': user, 'profile': profile, 'current_user': current_user, "projects": projects})
+    # view function for updating profile page
 
-  
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request, id):
+    current_user = request.user
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user_id = id
+            profile.save()
+            return redirect(home)
+    else:
+        form = UpdateProfileForm()
+    return render(request, 'profile/update_profile.html', {'user': user, 'form': form})
