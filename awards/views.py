3f9dfe3f6bb68e1cf4a925c54b566476dd9396eb
Juliet <chepngetichjuliet@gmail.com>
2019-07-31 15:56:50 +0300

    imports model classes and adds users

--- awards/views.py
+++ awards/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Project, Profile, Rating
+from django.contrib.auth.models import User
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def home(request):
-    return render(request, 'index.html')
+    users = User.objects.all()
+    current = request.user
+    projects = Project.objects.all()
+
+    return render(request, 'index.html', {'users': users, 'user': current, 'projects': projects})
 
 
 
